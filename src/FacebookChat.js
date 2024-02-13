c56fec6b42d8beb165e4a8fb9a9e7d82d9b8b524
vivekk2023 <vivek.k@kodehash.com>
2024-02-13 18:04:00 +0530

    updated

--- src/FacebookChat.js
+++ src/FacebookChat.js
@@ -3,7 +3,7 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 const FacebookChat = () => {
   return (
     <FacebookProvider appId="262731936859346" chatSupport>
-      <CustomChat pageId="100008229339895" minimized={true} />
+      <CustomChat pageId="104210531738590" minimized={true} />
     </FacebookProvider>
   );
 };
